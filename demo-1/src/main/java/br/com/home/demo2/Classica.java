559edc44f4a871e8fb56a440f18c8966ff3f9b2a
vsantos <vanderson.santos@prill.com.br>
2019-08-13 13:01:03 -0300

    Add method

---
+++ demo-1/src/main/java/br/com/home/demo2/Classica.java
@@ -0,0 +1,16 @@
+package br.com.home.demo2;
+
+
+public class Classica{
+	
+	/**
+	  * @return void
+	  * @param args - parametros de configuracao
+	  * 
+	  */
+	public static void doNothing(String[] args) {
+		
+		
+		
+	}
+}
